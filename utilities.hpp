3ea0b054ccfcbdd9ad3acd0c66609e80b9512ec1
NLuong127 <nluong@csu.fullerton.edu>
2016-11-28 00:34:46 -0800

    Update_01

--- utilities.hpp
+++ utilities.hpp
@@ -1,16 +1,20 @@
 #pragma once
 
+
 #include <fstream>
 #include <string>
 #include "SearchTree.h"
 
 
+using namespace std;
 SearchTree<Entry<string, string> >* buildBST_fromDatafile(const std::string& filename) {
 	std::ifstream f(filename.c_str());
 	if (!f)
 		throw std::runtime_error("cannot open data file");
 
+
 	std::string s;
+
 
 	getline(f, s);
 	std::string magic("CPSC131 Project3 raw data file");
@@ -18,23 +22,33 @@ SearchTree<Entry<string, string> >* buildBST_fromDatafile(const std::string& fil
 		throw std::runtime_error("incorrect data file");
 	getline(f, s);
 
+
 	std::string firstName, movie;
-	
+
+
 	SearchTree<Entry<string, string>> *bst = new SearchTree<Entry<string, string>>;
 
+
 	// TO DO: implement the creation of a binary search tree
-
-
+	for (int i = 3; i < 174; i++)
+	{
+		getline(f, s);
+		bst->insert(s.substr(0, s.find(",")), s.substr(s.find(",") + 1, s.find("  "))); //seperates s
+	}
 	f.close();
-
 	return bst;
 }
 
+
 // TO DO: You need to implement a function to print a singly linked list in reverse order
-void printInReverseOder(SNode<Entry<string,string> >* node)
+void printInReverseOder(SNode<Entry<string, string> >* node)
 {
+	SNode<Entry<string, string> >* temp = node;
+	while (temp->getnext())
+	{
+		cout << temp->getElem().key() << ", " << temp->getElem().value() << endl;
+		temp = temp->getnext();
+	}
 
 
 }
-
-
